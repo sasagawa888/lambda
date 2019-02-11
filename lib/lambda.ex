7e3d9a5b69a085708f598ce2ea815b52151a1e3d
kenichi <ken-s@tkcnf.or.jp>
2019-02-11 17:03:36 +0900

    add SKI combinator

--- lib/lambda.ex
+++ lib/lambda.ex
@@ -15,8 +15,8 @@ defmodule Lambda do
 
     def repl() do
       try do
-          #:io.write(read())
-          print(reduce(read()))
+          #:io.write(combinator(read()))
+          print(reduce(combinator(read())))
           repl()
       catch
         x -> IO.write(x)
@@ -29,6 +29,7 @@ defmodule Lambda do
     def read() do
       IO.gets("\n>") |> String.to_charlist |> parse([])
     end
+
 
     # terminal
     def parse('\n',res) do res end
@@ -43,44 +44,29 @@ defmodule Lambda do
       arg1 = String.to_atom(<<arg>>)
       [{:^,arg1,hd(parse([94|ls],[]))}]
     end
+    def parse([l|ls],res) when l >= 65 and l <= 122 do
+      #IO.inspect binding()
+      cond do
+        res == [] -> parse(ls,[String.to_atom(<<l>>)])
+        length(res) == 1 ->  parse(ls,res++[String.to_atom(<<l>>)])
+        true -> parse(ls,[res]++[String.to_atom(<<l>>)])
+      end
+    end
     # ( )
     def parse([40|ls],res) do
       #IO.inspect binding()
-      {exp,ls1} = parse1(ls,[])
+      {exp,ls1} = parse1([40|ls],[])
       cond do
         is_lambda(exp) -> parse(ls1,res++[exp])
         res == [] -> parse(ls1,exp)
         true -> parse(ls1,res++[exp])
       end
     end
-    def parse([41|ls],res) do
+    def parse(_,_) do
       #IO.inspect binding()
-      parse(ls,res)
+      throw "syntax error"
     end
-    def parse([l1,40|ls],[]) do
-      #IO.inspect binding()
-      parse([40]++ls,[String.to_atom(<<l1>>)])
-    end
-    def parse([l1,40|ls],res) do
-      #IO.inspect binding()
-      parse([40]++ls,[res]++[String.to_atom(<<l1>>)])
-    end
-    def parse([l1,l2|ls],res) when l2 < 48 do
-      #IO.inspect binding()
-      if res != [] and is_lambda(hd(res)) do
-        parse([l2]++ls,res++[String.to_atom(<<l1>>)])
-      else
-        parse([l2]++ls,[res]++[String.to_atom(<<l1>>)])
-      end
-    end
-    def parse([l1,l2|ls],[]) do
-      #IO.inspect binding()
-      parse(ls,[String.to_atom(<<l1>>),String.to_atom(<<l2>>)])
-    end
-    def parse([l1,l2|ls],res) do
-      #IO.inspect binding()
-      parse([l2]++ls,[res]++[String.to_atom(<<l1>>)])
-    end
+
 
     def parse1([94,arg,46|ls],res) do
       #IO.inspect binding()
@@ -88,26 +74,42 @@ defmodule Lambda do
       {body,ls1} = parse1(ls,res)
       {{:^,arg1,body},ls1}
     end
-    def parse1([94,arg|ls],res) do
+    def parse1([94,arg|ls],_) do
       #IO.inspect binding()
       arg1 = String.to_atom(<<arg>>)
-      {body,ls1} = parse1([94|ls],res)
-      {{:^,arg1,body},ls1}
+      {body,ls1} = parse1([94|ls],[])
+      {[{:^,arg1,body}],ls1}
     end
-    def parse1([40|ls],_) do
+    def parse1([l|ls],res) when l >= 65 and l <= 122 do
+      #IO.inspect binding()
+      cond do
+        res == [] -> parse1(ls,[String.to_atom(<<l>>)])
+        length(res) == 1 ->  parse1(ls,res++[String.to_atom(<<l>>)])
+        true -> parse1(ls,[res]++[String.to_atom(<<l>>)])
+      end
+    end
+    def parse1([40|ls],res) do
       #IO.inspect binding()
       {exp,[41|ls1]} = parse1(ls,[])
-      {exp,ls1}
+      cond do
+        res == [] -> {exp,ls1}
+        length(res) == 1 -> {res++[exp],ls1}
+        true -> {[res]++exp,ls1}
+      end
     end
-    # x )
-    def parse1([l1,l2|ls],_) when l2 < 48 do
+    def parse1([41|ls],res) do
       #IO.inspect binding()
-      {String.to_atom(<<l1>>),[l2]++ls}
+      {res,[41|ls]}
     end
-    def parse1([l1,l2|ls],_) do
+    def parse1('\n',res) do
       #IO.inspect binding()
-      {[String.to_atom(<<l1>>),String.to_atom(<<l2>>)],ls}
+      {res,'\n'}
     end
+    def parse1(_,_) do
+      #IO.inspect binding()
+      throw "syntax error1"
+    end
+
 
     def print([]) do end
     def print([{:^,x,y}]) do
@@ -116,14 +118,10 @@ defmodule Lambda do
       IO.write(".")
       print1(y)
     end
-    def print([x,y]) do
+    def print([x|xs]) do
       #IO.inspect binding()
       print1(x)
-      print([y])
-    end
-    def print([x]) when is_list(x) do
-      #IO.inspect binding()
-      print(x)
+      print1(xs)
     end
     def print([x]) do
       #IO.inspect binding()
@@ -139,9 +137,15 @@ defmodule Lambda do
       print1(y)
       IO.write(")")
     end
+    def print1([x,y]) when is_atom(x) and is_atom(y) do
+      IO.write("(")
+      IO.write(x)
+      IO.write(y)
+      IO.write(")")
+    end
     def print1([l|ls]) do
       print1(l)
-      print(ls)
+      print1(ls)
     end
     def print1(e) do
       #IO.inspect binding()
@@ -151,35 +155,50 @@ defmodule Lambda do
     def is_lambda({:^,_,_}) do true end
     def is_lambda(_) do false end
 
+
+    def combinator(:I) do {:^,:x,[:x]} end
+    def combinator(:K) do {:^,:x,[{:^,:y,[:x]}]} end
+    def combinator(:S) do {:^,:x,[{:^,:y,[{:^,:z,[:x,[:z,[:y,:z]]]}]}]} end
+    def combinator([x,y]) do
+      [combinator(x)]++[combinator(y)]
+    end
+    def combinator(x) do x end
+
     def reduce(:end) do
       throw "end"
     end
-    def reduce([x]) do [x] end
-    #IO.inspect binding()
-    def reduce([:I,y]) do
-      reduce([{:^,:x,:x},y])
-    end
     def reduce([x,y]) do
-    #IO.inspect binding()
-      if is_lambda(x) do
-        beta(x,y)
-      else
-        [x,y]
+      print([x,y])
+      IO.write('\n')
+      cond do
+        is_lambda(x) -> reduce(beta(x,y))
+        is_list(x) -> reduce([hd(reduce(x)),y])
+        true -> [x,y]
       end
+    end
+    def reduce(x) do
+      x
     end
 
     def beta({:^,arg,body},y) do
     #IO.inspect binding()
-      [replace(arg,body,y)]
+      replace(arg,body,y)
     end
 
-    def replace(x,x,z) when is_atom(x) do z end
-    def replace(_,y,z) when is_atom(y) do z end
+
     def replace(x,{:^,a,y},z) do
-      {:^,a,replace(x,y,z)}
+      exp = replace(x,y,z)
+      if is_list(exp) and length(exp) == 1 and is_lambda(hd(exp)) do
+        hd(exp)
+      else
+        {:^,a,replace(x,y,z)}
+      end
+    end
+    def replace(_,[],_) do [] end
+    def replace(x,[x|ys],z) do
+      [z] ++ replace(x,ys,z)
     end
     def replace(x,[y|ys],z) do
       [y] ++ replace(x,ys,z)
     end
-
 end
--- test/lambda_test.exs
+++ test/lambda_test.exs
@@ -5,20 +5,22 @@ defmodule LambdaTest do
   test "reduce test" do
     assert Lambda.reduce([:y]) == [:y]
     assert Lambda.reduce([:x, :y]) == [:x,:y]
-    assert Lambda.reduce([{:^,:x,:x}, :y]) == [:y]
+    assert Lambda.reduce([{:^,:x,[:x]}, :y]) == [:y]
   end
   test "parse test" do
     assert Lambda.parse('xy\n',[]) == [:x, :y]
     assert Lambda.parse('(xy)\n',[]) == [:x, :y]
-    assert Lambda.parse('^x.y\n',[]) == [{:^, :x , :y}]
-    assert Lambda.parse('^x.(^y.y)\n',[]) == [{:^, :x, {:^, :y, :y}}]
-    assert Lambda.parse('(^x.x)(^y.y)\n',[]) == [{:^ , :x , :x},{:^, :y, :y}]
+    assert Lambda.parse('^x.y\n',[]) == [{:^, :x , [:y]}]
+    assert Lambda.parse('^x.(^y.y)\n',[]) == [{:^, :x, {:^, :y, [:y]}}]
+    assert Lambda.parse('(^x.x)(^y.y)\n',[]) == [{:^ , :x , [:x]},{:^, :y, [:y]}]
     assert Lambda.parse('^x.xy\n',[]) == [{:^,:x,[:x,:y]}]
     assert Lambda.parse('xyz\n',[]) == [[:x,:y],:z]
     assert Lambda.parse('abcd\n',[]) == [[[:a,:b],:c],:d]
-    assert Lambda.parse('(^xy.x)\n',[]) == [{:^,:x,{:^,:y,:x}}]
-    assert Lambda.parse('(^xyz.x)\n',[]) == [{:^ ,:x,{:^,:y,{:^,:z,:x}}}]
+    assert Lambda.parse('(^xy.x)\n',[]) == [{:^,:x,{:^,:y,[:x]}}]
+    assert Lambda.parse('(^xyz.x)\n',[]) == [{:^ ,:x,[{:^,:y,{:^,:z,[:x]}}]}]
     assert Lambda.parse('(xy)z\n',[]) == [[:x,:y],:z]
     assert Lambda.parse('x(yz)\n',[]) == [:x,[:y,:z]]
+    assert Lambda.parse('a(b(cd))\n',[]) == [:a,[:b,[:c,:d]]]
+
   end
 end
